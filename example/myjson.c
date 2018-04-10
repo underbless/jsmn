e49c884d27a57d6e0f4ca2d68da824b0f9c6fc5a
underbless <mins830@naver.com>
2018-04-10 18:13:29 +0900

    myjson.c에 기능 추가

--- example/myjson.c
+++ example/myjson.c
@@ -27,7 +27,11 @@ int main() {
 	jsmntok_t t[128]; /* We expect no more than 128 tokens */
 
 	jsmn_init(&p);
-	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
+	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));//토큰 개수
+
+	#ifdef DEBUG_MODE
+		printf("Token's count : %d\n", r);
+	#endif
 	if (r < 0) {
 		printf("Failed to parse JSON: %d\n", r);
 		return 1;
@@ -43,16 +47,25 @@ int main() {
 	for (i = 1; i < r; i++) {
 		if (jsoneq(JSON_STRING, &t[i], "user") == 0) {
 			/* We may use strndup() to fetch string value */
+			#ifdef DEBUG_MODE
+			printf("%d %d\n[%d]", t[i+1].start, t[i+1].end, i+1);
+			#endif
 			printf("- User: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "admin") == 0) {
 			/* We may additionally check if the value is either "true" or "false" */
+			#ifdef DEBUG_MODE
+			printf("%d %d\n[%d]", t[i+1].start, t[i+1].end, i+1);
+			#endif
 			printf("- Admin: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "uid") == 0) {
 			/* We may want to do strtol() here to get numeric value */
+			#ifdef DEBUG_MODE
+			printf("%d %d\n[%d]", t[i+1].start, t[i+1].end, i+1);
+			#endif
 			printf("- UID: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
@@ -64,6 +77,9 @@ int main() {
 			}
 			for (j = 0; j < t[i+1].size; j++) {
 				jsmntok_t *g = &t[i+j+2];
+				#ifdef DEBUG_MODE
+				printf("[%d]", i+j+2);
+				#endif
 				printf("  * %.*s\n", g->end - g->start, JSON_STRING + g->start);
 			}
 			i += t[i+1].size + 1;
